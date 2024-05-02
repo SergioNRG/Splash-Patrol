acb27d5f7389cc1e85b5c1d9aeaf6fe0378f91e8
SergioNRG <129944863+SergioNRG@users.noreply.github.com>
2024-05-02 12:13:08 +0100

    AimState

--- Assets/player/PlayerScripts/PlayerAttacks.cs
+++ Assets/player/PlayerScripts/PlayerAttacks.cs
@@ -6,30 +6,46 @@ using UnityEngine;
 
 public class PlayerAttacks : MonoBehaviour
 {
+    private enum AimState
+    {
+        Idle,
+        Aiming,
+        NotAiming
+    }
+
+    private AimState _currentAimState;
+
     [Header("InputReaderSO")]
     [SerializeField] private InputReader _inputReader;
 
     [Header("Zoom Camera")]
     [SerializeField] private CinemachineVirtualCamera _cam;
 
+
     [Header("Camera Values To Adjust")]
-    [SerializeField] private float _aimFov = 30;
+    [SerializeField] private int _aimFov = 30;
+    [SerializeField] private int _normalFov = 60;
     [SerializeField] private float _zoomSpeed = 15;
-    [SerializeField] private float _normalFov = 60;
-    [SerializeField] private float _aimSpeedY = 50;
+    [SerializeField] private float _aimSpeedY = 30;
     [SerializeField] private float _aimSpeedX = 50;
     [SerializeField] private float _normalSpeedY ;
     [SerializeField] private float _normalSpeedX ;
 
-    private bool _isAiming;
 
+
+
+    //private bool _isAiming;
+
+    private float _timeToStopNotAiming = 0;
+  
     // Start is called before the first frame update
     void Start()
     {
+        _currentAimState = AimState.Idle;
         _normalSpeedX = _cam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed;
         _normalSpeedY = _cam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed;
         _cam.m_Lens.FieldOfView = _normalFov;
-        _isAiming = false;
+        //_isAiming = false;
         Debug.Log(_normalSpeedX);
 
     }
@@ -37,7 +53,34 @@ public class PlayerAttacks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        AimCheck();
+        switch (_currentAimState)
+        {
+            case AimState.Idle:
+                break;
+
+            case AimState.Aiming:
+                if (_cam.m_Lens.FieldOfView.ToString() != _aimFov.ToString())
+                {
+                    _timeToStopNotAiming = 0;
+                    _cam.m_Lens.FieldOfView = Mathf.Lerp(_cam.m_Lens.FieldOfView, _aimFov, _zoomSpeed * Time.deltaTime);
+                    _cam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed = _aimSpeedX;
+                    _cam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed = _aimSpeedY;
+                    Debug.Log("oioi");
+                }
+                break;
+
+            case AimState.NotAiming:
+                if (_cam.m_Lens.FieldOfView.ToString() != _normalFov.ToString())// _timeToStopNotAiming < 0.45f)
+                {
+                    _timeToStopNotAiming += Time.deltaTime;
+                    _cam.m_Lens.FieldOfView = Mathf.Lerp(_cam.m_Lens.FieldOfView, _normalFov, _zoomSpeed * Time.deltaTime);
+                    _cam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed = _normalSpeedX;
+                    _cam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed = _normalSpeedY;
+                    Debug.Log("fuifui");
+                }else { _currentAimState = AimState.Idle; }
+                break;
+        }
+        //AimCheck();
     }
 
     #region Input Events From SO
@@ -62,14 +105,11 @@ public class PlayerAttacks : MonoBehaviour
     #region METHODS THAT SUBSCRIBE THE EVENTS
     private void OnAim()
     {
-        _isAiming = true;
-        //_isNotAiming = false;
+        _currentAimState = AimState.Aiming;
     }
     private void OnAimCancelled()
     {
-        _isAiming = false;
-       // _isNotAiming = true;
-       // _cam.m_Lens.FieldOfView = _normalFov;
+        _currentAimState = AimState.NotAiming;
     }
 
 
@@ -87,21 +127,28 @@ public class PlayerAttacks : MonoBehaviour
 
     #region METHODS
 
-    private void AimCheck()
+   /* private void AimCheck()
     {
-        if (_isAiming && _cam.m_Lens.FieldOfView != _aimFov)
+        if (_isAiming && (int)_cam.m_Lens.FieldOfView > _aimFov)
         {
+            _timeToStop = 0;
             _cam.m_Lens.FieldOfView = Mathf.Lerp(_cam.m_Lens.FieldOfView, _aimFov, _zoomSpeed * Time.deltaTime);
             _cam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed = _aimSpeedX;
             _cam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed = _aimSpeedY;
+            Debug.Log("oioi");
         }
-        else if (!_isAiming && _cam.m_Lens.FieldOfView != _normalFov)
+        else if (!_isAiming && _timeToStop < 0.5f)
         {
+            _timeToStop += Time.deltaTime;
             _cam.m_Lens.FieldOfView = Mathf.Lerp(_cam.m_Lens.FieldOfView, _normalFov, _zoomSpeed * Time.deltaTime);
             _cam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed = _normalSpeedX;
             _cam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed = _normalSpeedY;
+            Debug.Log("fuifui");
         }
-    }
+    }*/
+
+ 
+
 
 
     #endregion
