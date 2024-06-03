8626c7efb6465bf3599f1e1bc3e87783cd32e198
SergioNRG <129944863+SergioNRG@users.noreply.github.com>
2024-06-03 14:31:03 +0100

    set damage colors

--- Assets/Enemies/Scripts/EnemyEffectsManager.cs
+++ Assets/Enemies/Scripts/EnemyEffectsManager.cs
@@ -24,7 +24,19 @@ public class EnemyEffectsManager : MonoBehaviour
     {
         if (_healthManager.CurrentHealth != 0 )
         {
-            ShowDamagePopUp();
+            if ( _healthManager.CurrentHealth >= _healthManager.MaxHealth/2)
+            {
+                ShowDamagePopUp(Color.green);
+            }
+            if (_healthManager.CurrentHealth < _healthManager.MaxHealth/2 && _healthManager.CurrentHealth > _healthManager.MaxHealth / 4)
+            {
+                ShowDamagePopUp(Color.yellow);
+            }
+            if (_healthManager.CurrentHealth <= _healthManager.MaxHealth / 4)
+            {
+                ShowDamagePopUp(Color.red);
+            }
+
             Debug.Log("enemy taking damage"); 
         }
         
@@ -33,15 +45,17 @@ public class EnemyEffectsManager : MonoBehaviour
     public void Die(Vector3 position)
     {
         Debug.Log("enemy morreu");
-        ShowDamagePopUp(); 
+        //ShowDamagePopUp(); 
         Destroy(gameObject);
     }
 
-    public void ShowDamagePopUp()
+    public void ShowDamagePopUp(Color color)
     {
         _offset = new Vector3(Random.Range(-1.5f, 1.5f), Random.Range(1f,2f),0);
         var popUp =  Instantiate(_floatingTxt, transform.position + _offset, Quaternion.identity,transform);
         popUp.transform.forward = Camera.main.transform.forward;
+        popUp.GetComponent<TextMeshPro>().faceColor = color;
         popUp.GetComponent<TextMeshPro>().text = _healthManager.CurrentHealth.ToString();
+        
     }
 }
