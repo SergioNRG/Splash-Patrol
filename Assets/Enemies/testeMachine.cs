02e62c9207999fb1feb2c5f602660c727ce24cf1
SergioNRG <129944863+SergioNRG@users.noreply.github.com>
2024-06-07 11:07:09 +0100

    try state machine

---
+++ Assets/Enemies/testeMachine.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class testeMachine : MonoBehaviour
+{
+    public enum State
+    {
+        Idle,
+        Attacking,
+    }
+
+    protected State _currentState = State.Idle;
+
+
+    public abstract void OnTriggerEnter(Collider other);
+
+    public abstract void OnTriggerExit(Collider other);
+
+    public abstract void Attack();
+
+    public void ChangeState(State newState)
+    {
+        if (_currentState != newState)
+        {
+            _currentState = newState;
+        }
+
+    }
+
+    public void StateMachine()
+    {
+        switch (_currentState)
+        {
+            default:
+            case State.Idle:
+
+
+                //can make some animation
+
+                break;
+
+            case State.Attacking:
+                Attack();
+                break;
+        }
+    }
+}
