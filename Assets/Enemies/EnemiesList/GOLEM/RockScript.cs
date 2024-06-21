f6a77a308901433a2956ce598df0698dd2d4d79a
SergioNRG <129944863+SergioNRG@users.noreply.github.com>
2024-06-21 12:22:34 +0100

    rock

--- Assets/Enemies/EnemiesList/GOLEM/RockScript.cs
+++ Assets/Enemies/EnemiesList/GOLEM/RockScript.cs
@@ -6,7 +6,8 @@ public class RockScript : MonoBehaviour
 {
     private Transform _playerTransform;
     private Rigidbody _rb;
-    [SerializeField] private float force;
+    [SerializeField] private float _force = 15;
+    [SerializeField] private float _rotationSpeed = 100.0f;
 
     private void Awake()
     {
@@ -16,13 +17,13 @@ public class RockScript : MonoBehaviour
 
     void Start()
     {
-        _rb.AddForce(transform.parent.forward * force, ForceMode.Impulse);
+        _rb.AddForce(transform.parent.forward * _force, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.Rotate(Vector3.right, _rotationSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
