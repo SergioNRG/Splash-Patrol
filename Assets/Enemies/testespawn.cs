7f9015dd2b239f9635b9e84925c32474809ac1ee
SergioNRG <129944863+SergioNRG@users.noreply.github.com>
2024-06-30 14:13:53 +0100

    teste spawn

---
+++ Assets/Enemies/testespawn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testespawn : MonoBehaviour
+{
+
+    public GameObject GameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+             Instantiate(gameObject);
+        }
+    }
+
+ 
+}
