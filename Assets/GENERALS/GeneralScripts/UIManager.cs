20687cb06767b2981f0f02b5a8d8d81ef4abe322
SergioNRG <129944863+SergioNRG@users.noreply.github.com>
2024-07-14 17:54:54 +0100

    main menu and UI manager

---
+++ Assets/GENERALS/GeneralScripts/UIManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SettingsPanel(GameObject panel)
+    {
+        if (!panel.activeInHierarchy)
+        {
+            panel.SetActive(true);
+        }
+        else { panel.SetActive(false); }
+    }
+
+    public void QuitGame()
+    {
+        // save any game data here
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
