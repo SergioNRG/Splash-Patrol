6b7ae5f51456bea22c88969562d41bf90997e330
SergioNRG <129944863+SergioNRG@users.noreply.github.com>
2024-07-26 18:06:44 +0100

    clean

--- Assets/GENERAL/GeneralScripts/GameManager.cs
+++ Assets/GENERAL/GeneralScripts/GameManager.cs
@@ -1,13 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
-
-    
-
     [SerializeField] private int _portalLife;
 
     public int Lvl = 1;
