cb8258c3230f5451174bf4b934343613af97d24f
SergioNRG <129944863+SergioNRG@users.noreply.github.com>
2024-07-26 17:32:39 +0100

    clean generals

--- Assets/GENERAL/GeneralScripts/AnimsList.cs
+++ Assets/GENERAL/GeneralScripts/AnimsList.cs
@@ -1,14 +1,10 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimsList : ScriptableObject
 {
     public List<Anim> Anims;
-
-    // public string IDLE;
-    // public string ATTACK;
 
     [Serializable]
     public class Anim
